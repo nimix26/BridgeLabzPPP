03d91af608d646a3cc5e1a5a191e022b1f5e56f8
nimix26 <149650796+nimix26@users.noreply.github.com>
2025-09-19 15:50:30 +0530

    Add files via upload

---
+++ OOPS/Level2/MovieTicket.java
@@ -0,0 +1,26 @@
+package OOPS.Level2;
+
+class MovieTicket {
+    private String movieName;
+    private int seatNumber;
+    private double price;
+
+    public void bookTicket(String movieName, int seatNumber, double price) {
+        this.movieName = movieName;
+        this.seatNumber = seatNumber;
+        this.price = price;
+    }
+
+    public void displayTicketDetails() {
+        System.out.println("Movie Name: " + movieName);
+        System.out.println("Seat Number: " + seatNumber);
+        System.out.println("Price: " + price);
+    }
+
+    public static void main(String[] args) {
+        MovieTicket ticket = new MovieTicket();
+        ticket.bookTicket("Avengers", 12, 250);
+        ticket.displayTicketDetails();
+    }
+}
+
---
+++ OOPS/Level2/PalindromeChecker.java
@@ -0,0 +1,27 @@
+package OOPS.Level2;
+
+class PalindromeChecker {
+    private String text;
+
+    public PalindromeChecker(String text) {
+        this.text = text;
+    }
+
+    public boolean isPalindrome() {
+        String reversed = new StringBuilder(text).reverse().toString();
+        return text.equalsIgnoreCase(reversed);
+    }
+
+    public void displayResult() {
+        if (isPalindrome()) {
+            System.out.println(text + " is a Palindrome");
+        } else {
+            System.out.println(text + " is NOT a Palindrome");
+        }
+    }
+
+    public static void main(String[] args) {
+        PalindromeChecker p = new PalindromeChecker("madam");
+        p.displayResult();
+    }
+}
